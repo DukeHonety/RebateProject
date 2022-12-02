038f8d4ed9e61d7864869b80ed42ef895f83ebe1
DukeHonety <mitchellcaleb00@gmail.com>
2022-12-02 11:30:45 +0900

    Remove ssl of backend

--- AppFrontend-master/src/components/Asidebar.jsx
+++ AppFrontend-master/src/components/Asidebar.jsx
@@ -173,7 +173,7 @@ export default function Asidebar() {
                     marginLeft: "20px",
                   }}
                   color={
-                    location.pathname === "/dashboard" ? "white" : "#8B8FA3"
+                    location.pathname === "/" ? "white" : "#8B8FA3"
                   }
                 >
                   Package Insert Gallery
@@ -305,9 +305,9 @@ export default function Asidebar() {
               paddingBottom: "10px",
             }}
             borderLeft={
-              location.pathname === "/dashboard" ? "3px solid #7335FD" : ""
+              location.pathname === "/" ? "3px solid #7335FD" : ""
             }
-            backgroundColor={location.pathname === "/dashboard" ? "black" : ""}
+            backgroundColor={location.pathname === "/" ? "black" : ""}
           >
             <Box
               component="img"
